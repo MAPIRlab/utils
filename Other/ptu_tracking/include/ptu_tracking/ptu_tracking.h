cd85f2ae451f95912567e012c76e73dee1bf1949
mapir-uma <mapir.uma@gmail.com>
2024-04-24 11:20:18 +0200

    updating PTU tracking - ongoing

--- Other/ptu_tracking/include/ptu_tracking/ptu_tracking.h
+++ Other/ptu_tracking/include/ptu_tracking/ptu_tracking.h
@@ -8,11 +8,9 @@
 #ifndef CrobotStatus_H
 #define CrobotStatus_H
 
-#include "rclcpp/rclcpp.hpp"
- // ptu d46
- // #include "ptu_interfaces/srv/set_pan_tilt.hpp"
- // #include "ptu_interfaces/srv/set_pan_tilt_speed.hpp"
- // #include "ptu_interfaces/msg/ptu.hpp"
+#include "rclcpp/rclcpp.hpp" 
+#include "ptu_tracking/PID.h"
+
  // ptu interbotix wxxms
 #include "interbotix_xs_msgs/msg/joint_group_command.hpp"
 
@@ -23,6 +21,7 @@
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include <std_msgs/msg/string.hpp>
+#include <apriltag_msgs/msg/april_tag_detection_array.hpp>
 // boost
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
@@ -32,34 +31,37 @@ class CptuTrack : public rclcpp::Node
 public:
     CptuTrack();
     ~CptuTrack();
-    void track();
 
     // Parameters
-    std::string tag_frame, ptu_frame, camera_frame;
-    std::string ptu_state_topic;
-    double loopRate;
+    int image_size_x, image_size_y;
+    std::string tag_frame, ptu_frame, tag_detection_topic;
+    float kp,ki,kd;
+    bool tf_based_tracking;
 
     // TF2
     std::unique_ptr<tf2_ros::Buffer> tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
     std::shared_ptr<tf2_ros::TransformListener> listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
 
-    // PTU D46 client
-    /*
-    rclcpp::Subscription<ptu_interfaces::msg::PTU>::SharedPtr ptu_sub;
-    rclcpp::Client<ptu_interfaces::srv::SetPanTilt>::SharedPtr pan_tilt_client;
-    rclcpp::Client<ptu_interfaces::srv::SetPanTiltSpeed>::SharedPtr speed_client;
-    std::shared_ptr<ptu_interfaces::srv::SetPanTilt::Request> request = std::make_shared<ptu_interfaces::srv::SetPanTilt::Request>();
-    */
+    // PID controller
+    std::shared_ptr<PID> pid_controller;
+    
     // PTU intebotix
     rclcpp::Publisher<interbotix_xs_msgs::msg::JointGroupCommand>::SharedPtr ptu_interbotix_pub;
+
+    // AprilTags
+    rclcpp::Subscription<apriltag_msgs::msg::AprilTagDetectionArray>::SharedPtr apriltag_sub;
 
 protected:
     // ptu_interfaces::msg::PTU current_ptu_state;
     // void ptuStateCallBack(const ptu_interfaces::msg::PTU::SharedPtr new_state);
-    bool initialized;
+    bool initialized;    
     bool ptu_d46;
     double current_ptu_pan, current_ptu_tilt;
+
+    void do_tf_based_tracking();
+    void do_image_based_tracking(float tag_x, float tag_y);
     void ptu_send_pan_tilt(float pan, float tilt);
+    void detected_tag_cb(apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr msg);
 };
 
 #endif
