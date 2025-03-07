dbdc97c3828f6c811440cbf3b12ef4872890deb3
Pepe Ojeda <ojedamorala@hotmail.com>
2025-03-07 13:30:48 +0100

    added parameter to control the tf publisher frequency

--- Other/tf_publisher_gui/Readme.md
+++ Other/tf_publisher_gui/Readme.md
@@ -4,4 +4,4 @@ It does what exactly you think it does.
 
 The GUI is rendered with [ament_imgui](https://github.com/MAPIRlab/utils/tree/ros2/Visualization), and lets you change the transformation in real time so you can use Rviz visualization to gradually adjust the parameters. Once you are happy, you can use the button to print the formatted params to the terminal.
 
-The resulting TF is published at 30Hz. Once you have correctly adjusted the transformation params, you can also run the node without the GUI by setting the `render` ROS param to `false`.
+The resulting TF is by default published at 30Hz, but you can adjust the frequency with the `freq` param. Once you have correctly adjusted the transformation params, you can also run the node without the GUI by setting the `renderGUI` ROS param to `false`.
--- Other/tf_publisher_gui/src/gui_pub.cpp
+++ Other/tf_publisher_gui/src/gui_pub.cpp
@@ -9,7 +9,7 @@ class GUIPub : public rclcpp::Node
 {
 public:
     GUIPub();
-    void Update();
+    void Run();
 
 private:
     void RenderGUI();
@@ -18,6 +18,7 @@ private:
     std::string parent_frame;
     std::string child_frame;
 
+    float freq;
     float x, y, z;
     float roll, pitch, yaw;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
@@ -29,18 +30,13 @@ int main(int argc, char** argv)
     rclcpp::init(argc, argv);
 
     auto node = std::make_shared<GUIPub>();
-    rclcpp::Rate rate(30);
-    while (rclcpp::ok())
-    {
-        node->Update();
-        rclcpp::spin_some(node);
-        rate.sleep();
-    }
+    node->Run();
 }
 
 inline GUIPub::GUIPub()
     : Node("guiPub")
 {
+    freq = declare_parameter<float>("freq", 30);
     x = declare_parameter<float>("x", 0);
     y = declare_parameter<float>("y", 0);
     z = declare_parameter<float>("z", 0);
@@ -54,72 +50,81 @@ inline GUIPub::GUIPub()
     tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
     render = declare_parameter<bool>("renderGUI", true);
-
-    if (render)
-        AmentImgui::Setup(
-            nullptr,
-            "TransformPublisher",
-            350,
-            250,
-            ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiConfigFlags_NavEnableKeyboard);
 }
 
-inline void GUIPub::Update()
+inline void GUIPub::Run()
 {
+    std::optional<std::jthread> renderThread;
     if (render)
-        RenderGUI();
+        renderThread.emplace(std::bind(&GUIPub::RenderGUI, this));
 
-    if (parent_frame == "" || child_frame == "")
-        return;
+    rclcpp::Rate rate(freq);
+    while (rclcpp::ok())
+    {
+        if (parent_frame == "" || child_frame == "")
+            return;
 
-    geometry_msgs::msg::TransformStamped transform;
-    transform.header.frame_id = parent_frame;
-    transform.header.stamp = now();
-    transform.child_frame_id = child_frame;
-    transform.transform.translation.x = x;
-    transform.transform.translation.y = y;
-    transform.transform.translation.z = z;
+        geometry_msgs::msg::TransformStamped transform;
+        transform.header.frame_id = parent_frame;
+        transform.header.stamp = now();
+        transform.child_frame_id = child_frame;
+        transform.transform.translation.x = x;
+        transform.transform.translation.y = y;
+        transform.transform.translation.z = z;
 
-    tf2::Quaternion quat;
-    quat.setRPY(roll, pitch, yaw);
-    transform.transform.rotation = tf2::toMsg(quat);
+        tf2::Quaternion quat;
+        quat.setRPY(roll, pitch, yaw);
+        transform.transform.rotation = tf2::toMsg(quat);
 
-    tf_broadcaster->sendTransform(transform);
+        tf_broadcaster->sendTransform(transform);
+        rate.sleep();
+    }
 }
 
 inline void GUIPub::RenderGUI()
 {
-    AmentImgui::StartFrame();
-
-    ImGuiIO& io = ImGui::GetIO();
-    ImGui::SetNextWindowSize({io.DisplaySize.x, io.DisplaySize.y});
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-
-    ImGui::Begin("Parameters");
-    ImGui::DragFloat("x", &x, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-    ImGui::DragFloat("y", &y, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-    ImGui::DragFloat("z", &z, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-
-    ImGui::DragFloat("roll", &roll, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-    ImGui::DragFloat("pitch", &pitch, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-    ImGui::DragFloat("yaw", &yaw, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
-
-    ImGui::InputText("parent_frame", &parent_frame);
-    ImGui::InputText("child_frame", &child_frame);
-
-    if (ImGui::Button("Print Current config"))
+    AmentImgui::Setup(
+        nullptr,
+        "TransformPublisher",
+        350,
+        250,
+        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiConfigFlags_NavEnableKeyboard);
+    rclcpp::Rate rate(30);
+    while (rclcpp::ok())
     {
-        fprintf(stderr, "{'x' :             %.2f},\n", x);
-        fprintf(stderr, "{'y' :             %.2f},\n", y);
-        fprintf(stderr, "{'z' :             %.2f},\n", z);
-        fprintf(stderr, "{'roll' :          %.2f},\n", roll);
-        fprintf(stderr, "{'pitch' :         %.2f},\n", pitch);
-        fprintf(stderr, "{'yaw' :           %.2f},\n", yaw);
-        fprintf(stderr, "{'parent_frame' :  '%s'},\n", parent_frame.c_str());
-        fprintf(stderr, "{'child_frame' :   '%s'},\n", child_frame.c_str());
+        AmentImgui::StartFrame();
+
+        ImGuiIO& io = ImGui::GetIO();
+        ImGui::SetNextWindowSize({io.DisplaySize.x, io.DisplaySize.y});
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+
+        ImGui::Begin("Parameters");
+        ImGui::DragFloat("x", &x, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+        ImGui::DragFloat("y", &y, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+        ImGui::DragFloat("z", &z, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+
+        ImGui::DragFloat("roll", &roll, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+        ImGui::DragFloat("pitch", &pitch, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+        ImGui::DragFloat("yaw", &yaw, 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+
+        ImGui::InputText("parent_frame", &parent_frame);
+        ImGui::InputText("child_frame", &child_frame);
+
+        if (ImGui::Button("Print Current config"))
+        {
+            fprintf(stderr, "{'x' :             %.2f},\n", x);
+            fprintf(stderr, "{'y' :             %.2f},\n", y);
+            fprintf(stderr, "{'z' :             %.2f},\n", z);
+            fprintf(stderr, "{'roll' :          %.2f},\n", roll);
+            fprintf(stderr, "{'pitch' :         %.2f},\n", pitch);
+            fprintf(stderr, "{'yaw' :           %.2f},\n", yaw);
+            fprintf(stderr, "{'parent_frame' :  '%s'},\n", parent_frame.c_str());
+            fprintf(stderr, "{'child_frame' :   '%s'},\n", child_frame.c_str());
+        }
+
+        ImGui::End();
+
+        AmentImgui::Render();
+        rate.sleep();
     }
-
-    ImGui::End();
-
-    AmentImgui::Render();
 }
